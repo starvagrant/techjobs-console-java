5aab887ddafde96d5aa908b77351f8f145323e6c
James Ginns <starvagrant@yahoo.com>
2020-01-12 15:29:51 -0600

    Print 'no results' when searches are empty

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -96,14 +96,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        System.out.println("*****");
-        for(HashMap jobEntry: someJobs) {
-
-            jobEntry.forEach((key, value1) -> {
-              //  Object value = value1;
-                System.out.println(key + ": " + value1);
-            });
-            System.out.println("*****");
+        if (someJobs.isEmpty()) {
+            System.out.println("No Results");
+        } else {
+            for (HashMap jobEntry : someJobs) {
+                System.out.println("*****");
+                jobEntry.forEach((key, value1) -> {
+                    //  Object value = value1;
+                    System.out.println(key + ": " + value1);
+                });
+                System.out.println("*****");
+            }
         }
     }
 }
